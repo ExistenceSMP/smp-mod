ea30d6fc85fd1f7521ef828ab77e708b4ab584b8
igalaxy <hey@igalaxy.dev>
2025-01-05 15:57:57 -0800

    init mod

---
+++ src/client/java/com/existencesmp/mod/ExistenceCommunityServerModClient.java
@@ -0,0 +1,10 @@
+package com.existencesmp.mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExistenceCommunityServerModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
