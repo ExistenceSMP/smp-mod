ca0a6ab728d25c6fdd16e42815188f19bee0909c
igalaxy <hey@igalaxy.dev>
2025-05-03 17:51:32 -0700

    add update notice

--- src/main/java/com/existencesmp/mod/SMPCommand.java
+++ src/main/java/com/existencesmp/mod/SMPCommand.java
@@ -1,10 +1,8 @@
 package com.existencesmp.mod;
 
 import com.mojang.brigadier.context.CommandContext;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import de.maxhenkel.admiral.annotations.Command;
 import de.maxhenkel.admiral.annotations.RequiresPermissionLevel;
-import net.minecraft.command.CommandSource;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
@@ -16,7 +14,11 @@ public class SMPCommand {
     public void resourcePackUpdate(CommandContext<ServerCommandSource> context) {
         try {
             SMPMod.downloadResourcePack(context.getSource().getServer(), true);
-            context.getSource().sendFeedback(() -> Text.literal("Successfully updated resource pack!"), false);
+            context.getSource().getServer().getPlayerManager().broadcast(
+                    Text.literal("An ").formatted(Formatting.GRAY)
+                            .append(Text.literal("Existence Community Resource Pack").formatted(Formatting.DARK_RED))
+                            .append(Text.literal(" update is available! Quit and re-join for it to take effect.").formatted(Formatting.GRAY)),
+                    false);
         } catch (Exception e) {
             context.getSource().sendError(Text.literal("Failed to update resource pack: " + e.getMessage()).formatted(Formatting.RED));
         }
